51735db177fcdefacd4b094646422e939fb06d5d
Jiaqi Xi <45897890+jiaqi-xi@users.noreply.github.com>
2021-06-16 16:19:08 +0800

    Add files via upload
    using dict, change distribution of a to [0.4, 0.6]

--- involution.py
+++ involution.py
@@ -2,15 +2,22 @@ import numpy as np
 import random
 import math
 import csv
+from copy import deepcopy
 
 
 np.random.seed(2021)
 m, n = 300, 100  # m个人，n个资源点
-a = np.random.rand(m)  # m个人的资源获取能力
-G = np.zeros((n, n))  # 网络图
-T = np.zeros((m, n))  # 每个人在每个节点上的时间t
-level = np.zeros((m, n))  # 每个人在节点上的个人水平值
+num_m = list(range(m))
+num_n = list(range(n))
+zero_n = dict(zip(num_n, np.zeros(n)))
+
+a = np.random.rand(m) / 5 + 0.4  # m个人的资源获取能力：0.4-0.6均匀分布
+a = dict(zip(num_m, a))
+G = dict.fromkeys(range(n), [])  # 网络
+T = dict.fromkeys(range(m), zero_n.copy())  # 每个人在每个节点上的时间t
+level = dict.fromkeys(range(m), zero_n.copy())  # 每个人在节点上的个人水平值
 loc = np.random.randint(n, size=m)  # 每个人的出生点
+loc = dict(zip(num_m, loc))
 
 
 def init_G(G):
@@ -20,7 +27,13 @@ def init_G(G):
         for j in range(e):
             if i == list[j]:
                 continue
-            G[i][list[j]] = G[list[j]][i] = 1
+            # 这样可以
+            temp = G[i].copy()
+            temp.append(list[j])
+            G[i] = temp.copy()
+
+            # 这样不行
+            # G[i].append(list[j])
     return G
 
 
@@ -35,20 +48,30 @@ def search(G):
     global loc
     max_iter = 100
     iter = 0
+
     while True:
 
         # 先更新累计时间和水平值
         for i in range(m):
             # 当前时刻T=1,2,...
-            T[i][loc[i]] += 1   # 在当前位置的时间+1
-            level[i][loc[i]] += a[i] * f(T[i][loc[i]]) * f(T[i][loc[i]])
+            temp = T[i].copy()
+            temp[loc[i]] += 1
+            T[i] = temp.copy()   # 在当前位置的时间+1
+            temp = level[i].copy()
+            temp[loc[i]] += a[i] * f(T[i][loc[i]])
+            level[i] = temp.copy()
 
         # 用更新后的水平值更新真实收益
         profit = []
         # 首先计算每个节点的总水平
-        total_level = np.sum(level, axis=0)
+        total_level = np.zeros(n)
+        for i in range(n):
+            for j in range(m):
+                total_level[i] += level[j][i]
         for i in range(m):
-            temp = np.divide(level[i] ** 2, total_level, out=np.zeros_like(total_level, dtype=np.float64), where=total_level != 0)
+            t = list(level[i].values())
+            k = np.array(t)
+            temp = np.divide(k ** 2, total_level, out=np.zeros_like(total_level, dtype=np.float64), where=total_level != 0)
             profit.append(np.sum(temp))
         if iter == 0:
             with open("total_profit.csv", "w") as csvfile:
@@ -65,7 +88,8 @@ def search(G):
             # 停留
             max_loc = cur
             max_prof = (level[i][cur] + a[i] * f(T[i][cur] + 1)) / (total_level[cur] + a[i] * f(T[i][cur] + 1))
-            neighbors = np.nonzero(G[cur])[0]
+            # neighbors = np.nonzero(G[cur])[0]
+            neighbors = G[cur]
             for j in range(len(neighbors)):
                 cur = neighbors[j]
                 prof = (level[i][cur] + a[i] * f(T[i][cur] + 1)) / (total_level[cur] + a[i] * f(T[i][cur] + 1))
