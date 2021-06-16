6138873de40a3b24b80b283a7e0458b5bc010788
chunfenri <yangliming2018@pku.edu.cn>
2021-06-16 14:25:10 +0800

    new

---
+++ try.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+f = open('./total_profit.csv','r')
+eachtime = []
+for i in range(100):
+    datas = f.readline()
+    data = datas.split(',')
+    print(data)
+    data = [float(i) for i in data]
+    eachtime.append(sum(data))
+print(eachtime)
+MA10 = []
+
+for i in range(10,len(eachtime)):
+    num = 0
+    for j in range(i-10,i):
+        num += eachtime[i]
+    num /= 10
+    MA10.append(num)
+
+plt.plot(eachtime)
+plt.show()
